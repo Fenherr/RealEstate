48a506ffde3bcf26e8a1b12fb33a1a079599e700
PATRICIA2487 <155555017+PATRICIA2487@users.noreply.github.com>
2024-02-13 16:47:57 +0100

    contact js

---
+++ asset/js/contact.js
@@ -0,0 +1,14 @@
+/*header commun -> JS*/
+
+let menuBurger1 = document.querySelector ("nav");
+    /*console.log (menuBurger1)*/
+let menuBurger2 = document.querySelector (".logo-mon-compte");
+    /*console.log (menuBurger2)*/
+
+let menuBurger = [menuBurger1, menuBurger2];
+    /*console.log (menuBurger)*/
+
+for (let i=0; i < menuBurger.length; i++) {
+    console.log (menuBurger[i])
+}
+
