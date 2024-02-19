d667a34f259f312eed71e8fd870afe72b3ef8939
PATRICIA2487 <155555017+PATRICIA2487@users.noreply.github.com>
2024-02-19 16:32:03 +0100

    contact

--- asset/js/contact.js
+++ asset/js/contact.js
@@ -1,18 +1,37 @@
-/*header commun -> JS*/
+/*header commun -> Menu burger => JS*/
 
-//let menuBurger1 = document.querySelector ("nav");
-    /*console.log (menuBurger1)*/
-//let menuBurger2 = document.querySelector (".logo-mon-compte");
-    /*console.log (menuBurger2)*/
+let burgerMenu1 = document.querySelector ("nav")
+/*console.log (burgerMenu1)*/
+let burgerMenu2 = document.querySelector (".logo-mon-compte");
+/*console.log (burgerMenu2)*/
 
-//let menuBurger = [menuBurger1, menuBurger2];
-    /*console.log (menuBurger)*/
+let burgerMenu = [burgerMenu1, burgerMenu2];
+/*console.log (burgerMenu)*/
 
-//for (let i=0; i < menuBurger.length; i++) {
-   // console.log (menuBurger[i])
-//}
+for (let i=0; i < burgerMenu.length; i++) {
+console.log (burgerMenu[i])
+}
 
-let select = document.getElementById("subject-select");
-console.log (select.value)
 
+
+
+/*formulaire -> JS*/
+/* -> gestion rechargement page suite submit*/
+/* -> pr chps obligatoires*/
+let form = document.querySelector (".form")
+form.addEventListener ("submit", (event) => {
+    event.preventDefault();
+console.log ("rechargement page non effectué")
+
+let actionForm1 = document.querySelector ("#name")
+console.log (actionForm1.value)
+let actionForm2 = document.querySelector ("#first-name")
+console.log (actionForm2.value)
+let actionForm3 = document.querySelector ("#email")
+console.log (actionForm3.value)
+let actionForm4 = document.querySelector ("#subject-select")
+console.log (actionForm4.value)
+let actionForm5 = document.querySelector ("#message")
+console.log (actionForm5.value)   
+})
 
