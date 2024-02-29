fb0b071d2ceec817a45d74fa6747d65c535c04e3
Yvialga <lesailesvirevoltantes@proton.me>
2024-02-29 11:47:41 +0100

    refactor header in php

---
+++ src/header.php
@@ -0,0 +1,19 @@
+<header class="flex justify-content align-item">    
+        <img class="logo" src="./asset/images/logo x150.png" alt="logo">
+        <nav>
+            <input type="checkbox" id="check">
+            <label for="check" class="checkbtn">
+                <i class="fas fa-bars" id="menu-opener"></i>
+                <i class="fa-solid fa-xmark" id="menu-closer" hidden></i>
+            </label>
+            <ul class="menu flex justify-content">
+                <li><a class="title-txt" href="buy.html">Acheter</a></li>
+                <li><a class="title-txt" href="rent.html">Louer</a></li>
+                <li><a class="title-txt" href="sell.html">Vendre</a></li>
+                <li><a class="title-txt" href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+        <div class="logo-mon-compte">
+            <a class="active" href="#Mon_compte"><i class="fa-solid fa-user"></i> Mon compte</a>
+        </div>
+    </header>
