b5530c1f19748e468ebcdf5eebb1dae1313502e7
Gornthorn <formati@netcourrier.com>
2024-03-04 13:32:56 +0100

    Mod

--- rent.php
+++ rent.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Location</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="./asset/css/rent.css">
     <link rel="stylesheet" href="./asset/css/header.css">
-    
+    <link rel="stylesheet" href="./asset/css/rent.css">
+    <script src="./asset/js/header.js" defer></script>
     <script src="./asset/js/rent.js" defer></script>
     
 </head>
