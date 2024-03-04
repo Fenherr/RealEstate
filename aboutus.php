512cbcf533d6fad1a4f44fd2ad6fd06c0ed293f2
marie36g <marie.formation.dev@gmail.com>
2024-03-04 13:48:35 +0100

    fds

--- aboutus.php
+++ aboutus.php
@@ -10,9 +10,9 @@
     <link rel="stylesheet" href="./asset/css/aboutus.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
         integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
-        <script src="asset/js/header.js" defer></script>
-        <script src="asset/js/aboutus.js" defer></script>
+        crossorigin="anonymous" referrerpolicy="no-referrer"/>
+        <script src="./asset/js/aboutus.js" defer></script>
+        <script src="./asset/js/header.js" defer></script>
 </head>
 
 <body>
