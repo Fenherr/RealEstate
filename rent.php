003eaa14d5d0f25891dde05d76ee548dff7fa000
Gornthorn <formati@netcourrier.com>
2024-03-04 13:46:31 +0100

    Modif z-index 2 in header_footers

--- rent.php
+++ rent.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Location</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
-    <link rel="stylesheet" href="./asset/css/header.css">
+    <link rel="stylesheet" href="./asset/css/header_footer.css">
     <link rel="stylesheet" href="./asset/css/rent.css">
     <script src="./asset/js/header.js" defer></script>
     <script src="./asset/js/rent.js" defer></script>
