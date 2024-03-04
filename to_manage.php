d08a3585cd124fda9bd0b9519f4eb587474b3fd2
Siblife <mbazeugeauddevweb@gmail.com>
2024-03-04 16:06:19 +0100

    Problem JS and media querie not finish

--- to_manage.php
+++ to_manage.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <link rel="stylesheet" href="asset/css/to_manage.css">   
     <link rel="stylesheet" href="asset/css/header_footer.css">
-    <script src="asset/js/header.js" defer></script>
+    <script src="./asset/js/header.js" defer></script>
 </head>
 <body>
     <!-- Header -->
