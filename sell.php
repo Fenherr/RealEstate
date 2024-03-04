f1a5a5e8bc9982a78fe756ca5d5099442d0b6474
Yvialga <lesailesvirevoltantes@proton.me>
2024-03-04 14:45:31 +0100

    restore fonts and add more details in js file

--- sell.php
+++ sell.php
@@ -4,8 +4,8 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer">
-        <link rel="stylesheet" href="asset/css/sell.css">
-        <link rel="stylesheet" href="asset/css/header.css">
+        <link rel="stylesheet" href="./asset/css/sell.css">
+        <link rel="stylesheet" href="./asset/css/header_footer.css">
         <title>Page de vente</title>
         <script src="./asset/js/sell.js" defer></script>
         <script src="./asset/js/header.js" defer></script>
