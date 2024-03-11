d1fd6ae373438bd918d05682abc887d901bfaeb8
Gornthorn <formati@netcourrier.com>
2024-03-11 21:31:15 +0100

    Add link css

--- asset/php/resultsearchrent.php
+++ asset/php/resultsearchrent.php
@@ -1,21 +1,24 @@
-<?php 
-    print_r($_POST);
-    $nomLocal="<br>Secteur : ".htmlspecialchars($_POST["local"]);
-    $nomPrize="<br>Prix de la location :  ".($_POST["prize"]);
-    $nomDistance="<br>Périmètre de recherche :  ".($_POST["distance"]);
-    echo $nomLocal;
-    echo $nomPrize;
-    echo $nomDistance;
- ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
 <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Location</title>
-    
+    <link rel="stylesheet" href="../css/header_footer.css">
+    <link rel="stylesheet" href="../css/rent.css">
 </head>
 <body>
-
+    <main>
+        <?php 
+            print_r($_POST);
+            $nomLocal="<p>Secteur : ".htmlspecialchars($_POST["local"])."</p>";
+            $nomPrize="<p>Prix de la location :  ".($_POST["prize"])."</p>";
+            $nomDistance="<p>Périmètre de recherche :  ".($_POST["distance"])."</p>";
+            echo $nomLocal;
+            echo $nomPrize;
+            echo $nomDistance;
+        ?>
+        <p style="text-align:center"><a href="../../rent.php"><button class="butt">Retour</button></a></p>
+    </main>
 </body>
 </html>
