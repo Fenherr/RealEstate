20ee058c9840f76d785972068d29cf744e55ff20
Gornthorn <formati@netcourrier.com>
2024-03-21 10:05:25 +0100

    Clean code

--- controller/formSearchEntriesRent.php
+++ controller/formSearchEntriesRent.php
@@ -1,14 +1,8 @@
 <?php
     // Recovery form entries 
     $nomPrize=((int)($_POST["prize"]));
-                //$nomDistance="<p>Périmètre de recherche :  ".($_POST["distance"])."</p>";
                 $nomLocal='"'.htmlspecialchars($_POST["local"]).'"';
                 $nomTypeHouse='"'.($_POST["habit"]).'"';
-                //echo $nomLocal;
-                //echo gettype($nomPrize);
-                //echo $nomPrize;
-                //echo $nomDistance;
-                //echo $nomTypeHouse;
     // Data connection
     require '../modele/connect_base.php';
     // Data processing
