4b2c18d54de9c8424efa2ad45c7b1b7dcfee7a76
marie36g <marie.formation.dev@gmail.com>
2024-03-22 10:25:06 +0100

    enferV4

--- vue/template/aboutus.php
+++ vue/template/aboutus.php
@@ -9,10 +9,10 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
     integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
     crossorigin="anonymous" referrerpolicy="no-referrer"/>
-    <script src="./asset/js/aboutus.js" defer></script>
+    <script src="../javascript/aboutus.js" defer></script>
     <link rel="stylesheet" href="../css/header_footer.css">
     <script src="../javascript/header.js" defer></script>
-    <link rel="stylesheet" href="../../asset/css/aboutus.css">
+    <link rel="stylesheet" href="../css/aboutus.css">
     <link rel ="icon" type="image/png" href= "./asset/images/logo_white_150.png">
 </head>
 
